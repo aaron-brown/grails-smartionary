255a832e805a79af6e8a07eda918915489f3efb4
Aaron Brown <brown.aaron.lloyd@gmail.com>
2013-11-02 21:32:55 -0500

    Updated doc link.

--- SmartionaryGrailsPlugin.groovy
+++ SmartionaryGrailsPlugin.groovy
@@ -12,7 +12,7 @@ and app can support both programmatic and administrative modification \
 of variable data.
 '''
 
-    def documentation = "http://grails.org/plugin/smartionary"
+    def documentation = "http://aaron-brown.github.io/grails-smartionary/docs/manual/index.html"
     def license = "APACHE"
     def issueManagement = [ system: "GITHUB", url: "https://github.com/aaron-brown/grails-smartionary/issues" ]
     def scm = [ url: "https://github.com/aaron-brown/grails-smartionary" ]
